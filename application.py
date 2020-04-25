1aacd9584832f6ff16932416dc58841b9fcf234a
Praqash <63649681+Praqash@users.noreply.github.com>
2020-04-25 06:17:29 +0530

    changed application.py added run.py

--- application.py
+++ application.py
@@ -5,22 +5,15 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-app = Flask(__name__)
-app.debug = True
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+from flask import Flask
 
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+my_awesome_app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-   return render_template('hello.html')
+@my_awesome_app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    my_awesome_app.run()
---
+++ run.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+app = Flask(__name__)
+app.debug = True
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route('/')
+def index():
+   return render_template('hello.html')
