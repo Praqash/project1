4505a5e572d58c75cc935d36ed909ad4521cba50
Praqash <63649681+Praqash@users.noreply.github.com>
2020-04-29 19:53:59 +0530

    made changes

--- application.py
+++ application.py
@@ -27,12 +27,12 @@ if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-db = SQLAlchemy(app)
+
 
 class user(db.Model):
     Name = db.Column(db.String(20), nullable=False)
