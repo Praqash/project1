fc83525a399902007548abadf0372b3a9eec3732
Praqash <63649681+Praqash@users.noreply.github.com>
2020-05-02 19:19:02 +0530

    commit

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import os
 from flask import render_template
+
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
@@ -15,21 +16,27 @@ from flask_wtf import FlaskForm
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from flask import render_template, url_for, flash, redirect, request
+from models import *
 
 
 
 
-
-
-
-
-from flask import Flask
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+def main():
+    db.create_all()
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
+
+
 
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
