2c4b7b66a88a9567ff92afdfd93a29d74917c083
Praqash <63649681+Praqash@users.noreply.github.com>
2020-05-21 21:24:49 +0530

    commit

--- create.py
+++ create.py
@@ -1,26 +1,12 @@
 import os
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
+from flask import Flask, render_template, request
+from flaskblog.models import *
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
 db.init_app(app)
-
-class User(db.Model, UserMixin):
-    __tablename__ = "User"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
-
 
 def table():
     db.create_all()
